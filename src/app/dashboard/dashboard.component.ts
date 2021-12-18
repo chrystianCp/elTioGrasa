fe465441e8d19c23c90eaafdc9afee871f031d99
DeathApex <chrystian.cardenas@outlook.com>
2021-12-17 22:11:08 -0600

    eliminar no utilizable

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,14 @@ import { Component, AfterViewInit } from '@angular/core';
 })
 export class DashboardComponent implements AfterViewInit {
   subtitle: string;
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   constructor() {
     this.subtitle = 'This is some text within a card block.';
   }
 
   ngAfterViewInit() {}
+
+
+  
 }
